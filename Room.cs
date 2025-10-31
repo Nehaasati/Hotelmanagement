ce5b33b878397283df5efdef421773c094322884
Nehaasati <31asatineha@gmail.com>
2025-10-31 01:42:27 +0100

    GuestName = define empty change no nullable to empty by default

--- Room.cs
+++ Room.cs
@@ -4,7 +4,7 @@ public class Room
 {
     public int Room_Number;
     public RoomStatus status;
-    public string? GuestName;// no nullable 
+    public string GuestName ="";// no nullable , empty by default
 
 }
 
