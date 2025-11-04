7f9022bd3da808daa424cf10d74c5d8ead5d57ff
Nehaasati <31asatineha@gmail.com>
2025-11-04 09:33:17 +0100

    add comment and add class as public

--- User.cs
+++ User.cs
@@ -1,12 +1,13 @@
-namespace App; 
+namespace App;
 
 
 // a user class to manage logning in and loggning out.
+// change to public class so we can access in program.cs and save user
 public class User
 {
-  
-   public string? UserEmail; // user's emailadress. 
-   public string? _password; // //  user's password. 
+
+  public string? UserEmail; // user's emailadress. 
+  public string? _password; // //  user's password. 
 
   public User(string? useremail, string? password) // a constructor 
   {
@@ -14,10 +15,10 @@ public class User
     _password = password; // assign the input password 
   }
 
-   
-  
+
+
   public bool TryLogin(string? useremail, string? password) // a boolean verification method to be called where want to check if the useremail and password are true. 
-                                                        
+
   {
     return useremail == UserEmail && password == _password;
   }
